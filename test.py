8ce5829d035fbef1eb037e606f8437ff76eaed81
MichalMate <matejicekmichal@seznam.cz>
2025-04-16 11:02:52 +0200

    komentar na 9 řádku

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 print("Zkouška gitu")
 
-print("Zkouška commit"
+print("Zkouška commit")
 
 print("zase2")
 
 print("Funguje;)")
+
+# udělám změny, uložím, dam do change +, popišu změnu, dám commit a aktualizuji na github
